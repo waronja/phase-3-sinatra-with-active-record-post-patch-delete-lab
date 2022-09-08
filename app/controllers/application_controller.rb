ad0c980d06ebeb7f5663f575c974de304a52faae
simon waronja <simon.waronja@student.moringaschool.com>
2022-09-08 07:56:39 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,34 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    # baked_good = BakedGood.new_baked_good
+    # baked_good.to_json
+
+    new_baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    new_baked_good.to_json  
+  end
+
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(
+      name: params[:name]
+    )
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+
+       baked_good = BakedGood.find(params[:id])
+       
+       baked_good.destroy
+       
+       baked_good.to_json
+  end
 end
